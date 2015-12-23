46c9dd197f2c9ca2e5daab8e51483eb0fdbe1b60
jmheyd <heydjm@comcast.net>
2015-12-22 20:29:46 -0600

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,18 @@
 # Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+  fibonacci_array = []
+  fibonacci_array[0] = 1
+  fibonacci_array[1] = 2
+  i = 1
+  #while i < limit
+  while fibonacci_array[i] < limit
+    fibonacci_array[i + 1] = fibonacci_array[i] + fibonacci_array[i-1]
+    i += 1 
+  end
+  fibonacci_array = fibonacci_array.select {|x| x < limit}
+  even_fibonacci_array = fibonacci_array.select {|x| x.even?}
+  sum_even = even_fibonacci_array.inject(:+)
+  return sum_even
+end
+
+ 
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,24 @@
 # Implement your object-oriented solution here!
+ class EvenFibonacci
+
+  def initialize(limit, fibonacci_array = nil)
+    @limit = limit
+    @fibonacci_array = fibonacci_array || Array.new
+  end
+
+  def sum
+    @fibonacci_array = []
+    @fibonacci_array[0] = 1
+    @fibonacci_array[1] = 2
+    i = 1
+    while @fibonacci_array[i] < @limit
+      @fibonacci_array[i + 1] = @fibonacci_array[i] + @fibonacci_array[i-1]
+      i += 1 
+    end
+    @fibonacci_array = @fibonacci_array.select {|x| x < @limit}
+    even_fibonacci_array = @fibonacci_array.select {|x| x.even?}
+    sum_even = even_fibonacci_array.inject(:+)
+    return sum_even
+  end
+end
+  
